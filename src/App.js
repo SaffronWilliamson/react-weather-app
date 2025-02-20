217934ccc049b44091c98cd5c52dcc68da4e4298
SaffronWilliamson <saffron.havana99@gmail.com>
2025-02-20 14:07:08 +0000

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{' '}
+        <a
+          href="https://www.linkedin.com/in/saffron-williamson/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Saffron Williamson
+        </a>
+        , open sourced on{' '}
+        <a
+          href="https://github.com/SaffronWilliamson/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+        , and hosted on{' '}
+        <a
+          href="https://react-weather-app-saffron-williamson.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
