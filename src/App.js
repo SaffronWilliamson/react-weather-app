f8bbafb7ff85a513cacf2992cecd922edfaf7327
SaffronWilliamson <saffron.havana99@gmail.com>
2025-02-20 15:37:05 +0000

    Added boostrap

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
+import React from 'react';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{' '}
-        <a
-          href="https://www.linkedin.com/in/saffron-williamson/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Saffron Williamson
-        </a>
-        , open sourced on{' '}
-        <a
-          href="https://github.com/SaffronWilliamson/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-        , and hosted on{' '}
-        <a
-          href="https://react-weather-app-saffron-williamson.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{' '}
+          <a
+            href="https://www.linkedin.com/in/saffron-williamson/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Saffron Williamson
+          </a>
+          , open sourced on{' '}
+          <a
+            href="https://github.com/SaffronWilliamson/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          , and hosted on{' '}
+          <a
+            href="https://react-weather-app-saffron-williamson.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
