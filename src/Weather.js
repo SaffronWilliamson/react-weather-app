8fdcf366f37a547e35928bd79b46f646c16720cf
SaffronWilliamson <saffron.havana99@gmail.com>
2025-02-21 14:17:14 +0000

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city.."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -23,14 +28,21 @@ export default function Weather() {
         <li>Wednesday 07:00</li>
         <li>Scattered showers</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/rain_s_cloudy.png"
-            alt="Scattered showers"
-          />
-          6ºC
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/rain_s_cloudy.png"
+              alt="Scattered showers"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">6</span>
+              <span className="unit">ºC</span>
+            </div>
+          </div>
         </div>
+
         <div className="col-6">
           <ul>
             <li>Precipitation: 55%</li>
